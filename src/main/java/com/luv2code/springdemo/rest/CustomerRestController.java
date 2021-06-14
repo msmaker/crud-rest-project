9d1650c85a52c2d1fd9507e0b15c7a05e6306bfc
Marco Silva <marcosilva.tec@gamil.com>
2021-06-14 15:09:57 -0300

    DELETE

--- src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
+++ src/main/java/com/luv2code/springdemo/rest/CustomerRestController.java
@@ -3,9 +3,11 @@ package com.luv2code.springdemo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,10 +49,36 @@ public class CustomerRestController {
 
 		// also just in case the pass an id JSON ... set id to 0
 		// this is force a save of new item ... instead of update
-		
+
 		theCustomer.setId(0);
 		customerService.saveCustomer(theCustomer);
 
 		return theCustomer;
+	}
+
+	// add mapping for PUT /customers - update existing customer
+
+	@PutMapping("/customers")
+	public Customer updateCustomer(@RequestBody Customer theCustomer) {
+
+		customerService.saveCustomer(theCustomer);
+
+		return theCustomer;
+	}
+
+	// add mapping for DELETE /customer/{customerId} - delete customer
+	@DeleteMapping("/customers/{customerId}")
+	public String deletetCustomer(@PathVariable int customerId) {
+
+		Customer tempCustomer = customerService.getCustomer(customerId);
+
+		// throw exception if null
+		if (tempCustomer == null) {
+			throw new CustomerNotFoundException("Customer id not found - " + customerId);
+		}
+		
+		customerService.deleteCustomer(customerId);
+
+		return "Deleted customer id - " + customerId;
 	}
 }
